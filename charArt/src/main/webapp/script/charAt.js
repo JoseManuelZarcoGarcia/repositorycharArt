5cc2b5fe917fe52138d3b3cf32ce2de06a411b89
DESKTOP-3198Q49\usuario <jose.manuel.zarco.garcia@gmail.com>
2023-01-30 09:48:56 +0100

    version inicial

---
+++ charArt/src/main/webapp/script/charAt.js
@@ -0,0 +1,10 @@
+
+
+function holaMundo(){
+let muchoTexto = "Hola mundo, que tal?"
+
+     document.write("El carácter en el índice 0 es '" + muchoTexto.charAt(0));
+     document.write("El carácter en el índice 5 es '" + muchoTexto.charAt(5));
+     document.write("El carácter en el índice 3 es '" + muchoTexto.charAt(3));
+      
+}
